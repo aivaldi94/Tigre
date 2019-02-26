ff6142ca0595e03bebf07ef6f5b512811343b9ef
aivaldi94 <angie.ivaldi@gmail.com>
2019-02-26 19:20:06 -0300

    arreglo mala impresion y subo nuevo script

--- entrega3/pruebaAsm.sh
+++ entrega3/pruebaAsm.sh
@@ -4,18 +4,21 @@
 echo "HAGO MAKE" > resultado.txt
 make 
 
-for (( i=1; i<=19; i++))
-	do			
-			echo "TEST "$i; 			
-			./tiger ../tests/good/test$i.tig -asm 2>&1 >/dev/null;			
-			echo "**********************************************" >> resultado.txt;
-			echo "TEST "$i >> resultado.txt;			
-			gcc -s ../tests/TestAssm/prueba.s ../tests/TestAssm/runtime.o;					
-			./a.out &>> resultado.txt; 
-			if [ "$?" -eq 139 ];
-			then echo "segfault" &>> resultado.txt;
-		        fi;
-			echo " " >> resultado.txt;
+for (( i=1; i<=24; i++))
+	do
+		# si es el test 20 (merge) lo ignoro porque hay que pasarle los argumentos
+		if [ $i -ne 20 ];			
+			then echo "TEST "$i; 			
+				./tiger ../tests/good/test$i.tig -asm 2>&1 >/dev/null;			
+				echo "**********************************************" >> resultado.txt;
+				echo "TEST "$i >> resultado.txt;			
+				gcc -s ../tests/TestAssm/prueba.s ../tests/TestAssm/runtime.o;					
+				./a.out &>> resultado.txt; 
+				if [ "$?" -eq 139 ];
+				then echo "segfault" &>> resultado.txt;
+				fi;
+				echo " " >> resultado.txt;
+			fi;
 	done
 
 echo "TERMINO!"
